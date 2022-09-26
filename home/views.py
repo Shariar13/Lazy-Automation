4654e6738d9362e8d44c66c17e31c64c56c5a72d
Shariar13 <shariar15-2414@diu.edu.bd>
2022-09-26 23:35:24 +0600

    add search youtube in views.py

--- home/views.py
+++ home/views.py
@@ -6,7 +6,11 @@ from django.views.generic import CreateView, DetailView
 from django.urls import reverse_lazy
 from django.urls import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def home(request):
@@ -14,3 +18,16 @@ def home(request):
 
 def virtual_assistant (request):
     return render (request, "virtual_assistant.html")
+
+def search_youtube (request):
+    if request.method == "GET":
+        video = request.GET ['search']
+    driver = webdriver.Chrome()
+    driver.get('https://www.youtube.com/results?search_query={}'.format(str(video)))
+    while(True):
+       pass
+    
+
+    # # play the video
+    # wait.until(visible((By.ID, "video-title")))
+    # driver.find_element_by_id("video-title").click()
